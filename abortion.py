2aca68ea20cc7c65e157e4268b251de90451c29f
namlehai90 <namlehai90@gmail.com>
2018-06-25 14:43:31 -0600

    update data and code
    Santa Fe CSSS18 Group Project on Converstation Dynamics

---
+++ abortion.py
@@ -0,0 +1,249 @@
+# -*- coding: utf-8 -*-
+
+#------ Processing the labels of the raw IMDB data --------+
+import os
+
+#import sys   
+#from imp import reload
+
+#reload(sys)
+#sys.setdefaultencoding("utf-8")
+#sys.setdefaultencoding('utf8')
+
+imdb_dir = 'abortion'
+train_dir = os.path.join(imdb_dir, 'train1')
+
+labels = []
+texts = []
+
+for label_type in ['pro', 'against']:
+    dir_name = os.path.join(train_dir, label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname), encoding="utf-8")
+            for line in f:
+                texts.append(line)
+                if label_type == 'against':
+                    labels.append(0)
+                else:
+                    labels.append(1)
+#            line = f.readline()
+#            texts.append(f.read())
+#            texts.append(line)
+#            print(len(line))
+#            print(texts)
+            f.close()
+            # if neg, label = 0; else: label = 1
+#            if label_type == 'against':
+#                labels.append(0)
+#            else:
+#                labels.append(1)
+                
+print("length of the texts ", len(texts))
+print("length of the labels ", len(labels))
+
+
+#------------ TOKENIZING THE DATA -----------_+
+"""Tokenizing the text of the raw IMDB data
+"""
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+np.random.seed(1)
+
+# cuts off reviews after 100 words
+maxlen = 200
+
+# trains on 10000 samples
+training_samples = 10000
+
+# validates on 10000 samples
+validation_samples = 10183
+
+# considers only the top 10000 words in the dataset
+max_words = 20000
+
+tokenizer = Tokenizer()#(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+
+labels = np.asarray(labels)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+data = pad_sequences(sequences, maxlen=maxlen)
+
+labels = np.asarray(labels)
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+
+# Split the data into a training set and a validation set
+# But first, shuffle the data, since we started from data
+# where sample are ordered (all negative first, then all positive).
+indices = np.arange(data.shape[0])
+print(indices)
+np.random.shuffle(indices)
+print(indices)
+data = data[indices]
+labels = labels[indices]
+
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples: training_samples + validation_samples]
+y_val = labels[training_samples: training_samples + validation_samples]
+#x_val = data[:training_samples]
+#y_val = labels[:training_samples]
+
+
+print(x_train.shape)
+#print(x_train)
+print(x_val.shape)
+
+embedding_dim = 200
+
+'''
+# Pretrained-glove
+
+glove_dir = 'glove'
+
+embeddings_index = {}
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'), encoding='utf-8')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+embedding_dim = 100
+
+embedding_matrix = np.zeros((max_words, embedding_dim))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if i < max_words:
+        if embedding_vector is not None:
+            # Words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+                            
+'''
+                            
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
+
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(200, activation='relu'))
+model.add(Dense(200, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=32,
+                    validation_data=(x_val, y_val))
+
+model.save_weights('pre_trained_glove_model.h5')
+
+
+print(history.history.keys())
+
+#-------- PLOTTING -----------+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+#---------- PRETRAINED ---------------+
+
+'''
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
+
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=32,
+                    validation_data=(x_val, y_val))
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()'''
+
+
+'''
+# TEST
+test_dir = os.path.join(imdb_dir, 'test')
+
+labels = []
+texts = []
+
+for label_type in ['pro', 'against']:
+    dir_name = os.path.join(test_dir, label_type)
+    for fname in sorted(os.listdir(dir_name)):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname), encoding='utf-8')
+            texts.append(f.read())
+            f.close()
+            if label_type == 'against':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+sequences = tokenizer.texts_to_sequences(texts)
+x_test = pad_sequences(sequences, maxlen=maxlen)
+y_test = np.asarray(labels)'''
